9155e6cd1f9213b793781bc25b45a96106e20b3c
sujanpokhr <67049749+sujanpokhr@users.noreply.github.com>
2025-09-21 08:13:34 +0545

    Add files via upload

---
+++ Lab1/lab1.js
@@ -0,0 +1,6 @@
+let fahrenheit = 55; //this a average temperature in Kathmandu(capital city of nepal) in the month of september//
+console.log("Temperature (Fahrenheit): " + fahrenheit);//printing the temperature in fahrenheit//
+let celsius = (fahrenheit - 32) * 5 / 9; //printing the temperature in celsius//
+console.log("Temperature (Celsius): " + celsius);//formula to convert fahrenheit to celsius//
+let kelvin = (fahrenheit + 459.67) * 5 / 9; //formula to convert fahrenheit to kelvin//
+console.log("Temperature (Kelvin): " + kelvin);//printing the temperature in kelvin//
